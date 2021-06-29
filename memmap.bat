2ae1e911b2f3d8366d5901122f6ada8d13db8cf7
Samuel-DEVULDER <samuel.devulder@laposte.net>
2021-06-30 00:35:50 +0200

    updated with new command-line arguments

--- memmap.bat
+++ memmap.bat
@@ -11,7 +11,7 @@ if "%~1"=="-FIXED_CTRL_C" (
 )
 
 echo Press ctrl-c to break
-%~dsp1\lua.exe %~dsp1\memmap.lua -loop -html -map
+%~dsp1\lua.exe %~dsp1\memmap.lua -loop -html -map -equates -mach=?? 
 if exist memmap.html (
 	echo Showing result in HTML...
 	start memmap.html
